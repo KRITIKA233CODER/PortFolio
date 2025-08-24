923c8a7cf85f1f68ca4cf72a9de475e3ff29b398
KRITIKA233CODER <kritikaniranjandangi@gmail.com>
2025-08-24 16:41:48 +0530

    second commit

--- src/constants.js
+++ src/constants.js
@@ -137,7 +137,7 @@ export const education = [
     id: 0,
     img: VitLogo,
     school: "Vellore Institute of Technology, Bhopal",
-    date: "Sept 2022 - July 2024",
+    date: "Sept 2023 - Sept 2027",
     grade: "8.78 CGPA",
     desc: "Pursuing my 'Bachelor's degree (B.Tech) in Computer Science & Engineering' from Vellore Institute of Technology, Bhopal. During my time at VIT, I gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively participated in various workshops and technical events, which enhanced my skills and knowledge. My experience at VIT has been instrumental in shaping my technical abilities and professional growth.",
     degree: "Bachelor of Technology - B.Tech",
